5a663ea533bd2c21cc5a3c9d9877ae5d739ef448
alisalali <alis.alali@hotmail.com>
2024-10-08 01:48:00 +0300

    lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((currActivePlyer) => (currActivePlyer === "X" ? "O" : "X"));
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="player 1" symbol="X" />
-          <Player initialName="player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard/>
+        <GameBoard
+          activePlayerSymbol={activePlayer}
+          onSelectSquare={handleSelectSquare}
+        />
       </div>
       LOG
     </main>
